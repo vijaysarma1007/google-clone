396d99de58478eb7c0995a32556eb743314f72e9
vijaysarma1007 <svmsarma53@gmail.com>
2024-01-07 22:57:22 +0530

    added body

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
 import React from "react";
 import HomeHeader from "@/components/HomeHeader";
-
+import Image from "next/image";
+import HomeSearch from "@/components/HomeSearch";
 export default function Home() {
   return (
     <>
+      {/* Header */}
       <HomeHeader />
+
+      {/* body */}
+      <div className="flex flex-col items-center mt-24">
+        <Image src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/640px-Google_2015_logo.svg.png" width="300" height="100" alt="google icon" />
+
+        <HomeSearch />
+      </div>
     </>
   );
 }

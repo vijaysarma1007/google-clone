fd0c331758d7c144888dc363bb8926d9d3b9a7e7
vijaysarma1007 <svmsarma53@gmail.com>
2024-01-12 17:46:46 +0530

    added error handler for search and request

--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,11 +1,31 @@
+import Link from "next/link";
 import React from "react";
 const API_KEY = process.env.API_KEY;
 const CONTEXT_KEY = process.env.CONTEXT_KEY;
+
 export default async function WebSearchPage({ searchParams }) {
   const response = await fetch(`https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${searchParams.searchTerm}`);
-  const data = await response.json();
-  console.log(data);
-  const results = data.items;
 
-  return <>{results && results.map((result) => <h1>{result.title}</h1>)}</>;
+  if (!response.ok) {
+    throw new Error("Something went wrong");
+  }
+
+  const data = await response.json();
+  // console.log(data);
+  const results = data.items;
+  if (!results) {
+    return (
+      <div className="flex flex-col justify-center items-center pt-10">
+        <h1 className="text-3xl mb-4">No results found</h1>
+        <p className="text-lg">
+          Try searching for something else or go back to the homepage{" "}
+          <Link href={"/"} className="text-blue-500">
+            HOME
+          </Link>
+        </p>
+      </div>
+    );
+  }
+
+  return <>{results && results.map((result) => <h1 key={result.title}>{result.title}</h1>)}</>;
 }
